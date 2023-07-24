aaa1b0a1e0b6bc91287c7ea08155b42b41576dea
Youngmanbo <yo900326@gmail.com>
2023-07-24 11:14:03 +0900

    파일추가 js_2.js 네비이게이션바 클릭 기능 추가

---
+++ js_2.js
@@ -0,0 +1,19 @@
+let search = document.getElementById();
+
+search.addEventListener('click',(e)=>{
+
+} )
+
+let nav = document.getElementById();
+
+nav.onclick = function(){
+   let nav_bar = document.getElementById();
+   let display = nav_bar.style.display;
+   if(display === 'none'){
+        display = 'flex';
+        nav_bar.style.direction ='row';
+
+   }else{
+        display = 'none';
+   }
+}
