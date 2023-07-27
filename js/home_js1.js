8ea38a842e78f870eba3ec09c0b97b0f38e65682
kimziho <rlawlgh1027@naver.com>
2023-07-27 11:34:03 +0900

    비디오 불러오기 실패

--- js/home_js1.js
+++ js/home_js1.js
@@ -1,53 +1,14 @@
-// ulr을 제외한 비디오 정보들
-// function getVideoInfo(n){    // n번째 동영상 info 가져오기
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('GET', `<http://oreumi.appspot.com/video/getVideoInfo?vidie_id=${n}>`, true);
-//     xhr.onreadystatechange = function(){
-//         if (xhr.status === 200 && xhr.readyState === 4){
-//             // 값을 잘 받아왔을 때
-//             return JSON.parse(xhr.responseText) ;
-//         }else{
-//             // 요청이 실패한 경우
-//             console.error('Error:', xhr.status);
-//         }
-//     };
-// }
-
-//url받아오기
-function getVideoUrl(n) {
-  // n번째 동영상 info 가져오기
-  const url = new XMLHttpRequest();
-  url.open('GET', `http://oreumi.appspot.com/video/getVideoInfo?video_id=${n}`, true);
-  url.onreadystatechange = function() {
-    if (url.status === 200 && url.readyState === 4) {
-      // 값을 잘 받아왔을 때
-      return JSON.parse(url.responseText);
-    } else {
-      // 요청이 실패한 경우
-      console.error('Error:', url.status);
-    }
-  };
-  url.send();
-}
-//console.log(videoUrls);
-
+// import * as res from './requests.js';
 
 // 비디오 넣을 태그 자리 불러옴
 const videoContainer=document.querySelector(".body-container");
 
 let videoList = [];
 let videoUrls = [];
-<<<<<<< HEAD
-let videoList = [];
-=======
->>>>>>> 4f483618864a6a9866f37f3aca783fa0c207e5a0
 
 function renderVideoList(videoList) {
   videoContainer.innerHTML = ''; // 기존 비디오 목록 초기화
   videoList.forEach((video, index) => {
-    // videoUrls받아오는거 아직안됨
-    // let videoUrls=getVideoUrl(index);
-    // console.log(videoUrls);
     
     let videoElement = document.createElement('video');
     let videoInfoIndex = videoUrls[video.video_id];
@@ -69,25 +30,18 @@ function renderVideoList(videoList) {
   });
 }
 
+async function getVideo(id){
+  url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
 function makeurlList(videoList){
-  videoList.forEach((video) => {
-    const videoUrl = new XMLHttpRequest();
-    let requestUrl = "http://oreumi.appspot.com/video/getVideoInfo?video_id=";
-    requestUrl += video.video_id
-    videoUrl.open('GET' , requestUrl, true);
-
-    videoUrl.onreadystatechange = function() {
-      if(videoUrl.status === 200 && video.readyState === 4) {
-        let videoUrlList = JSON.parse(videoUrl.responseText);
-        videoUrls[video.video_id] = videoUrlList;
-      }else{
-        console.error('Error:' , videoUrl.status);
-      }
-    };
-    videoUrl.send();
-
+  videoList.forEach((video, index) => {
+    videoUrls[index]=(getVideo(index));
   });
 }
+
 
 function getVideoList() {
   const temp = new XMLHttpRequest();
