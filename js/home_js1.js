1f53357b8913408ffbade5f6ee181c57bf8aa325
kimziho <rlawlgh1027@naver.com>
2023-07-27 14:05:35 +0900

    Add gitignore

--- js/home_js1.js
+++ js/home_js1.js
@@ -75,7 +75,48 @@ function getVideoList() {
   temp.send();
 }
 
+
+
+async function getChannel(param=undefined){
+  Url = 'http://oreumi.appspot.com/channel/getChannelVideo?video_channel=oreumi'
+  const response = await fetch(Url,{
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+  });
+  return response.json();
+}
+
+function getVideo(id){
+  let url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
+  return fetch(url).then(res=> res.json());
+}
+
+async function getVideo2(id){
+  url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+async function getVideoList(){
+  url = 'http://oreumi.appspot.com/video/getVideoList';
+  const response = await fetch(url);
+  return response.json();
+}
+
+async function getVideoInfoList(res){
+
+  const prom = Object.keys(res).map(async key => {
+      return await getVideo2(res[key].video_id);
+  })
+  const result = await Promise.all(prom);
+
+  return result;
+}
+
 window.onload = function(){ // (window == 브라우저) 기본적인 html이 다 로드되면 안에있는 함수를 실행하겠다는 뜻
+  videoUrls = getVideoInfoList(getChannel())
   getVideoList();
   
   // getVideoUrl(0); 
