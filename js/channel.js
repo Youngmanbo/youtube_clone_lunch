7750b491790843d9e451fec7ed8c426542ddec17
Youngmanbo <yo900326@gmail.com>
2023-07-27 10:53:23 +0900

    dd

--- js/channel.js
+++ js/channel.js
@@ -1,25 +1,25 @@
-// async function getChannel(param=undefined){
-//     Url = 'http://oreumi.appspot.com/channel/getChannelVideo?video_channel=oreumi'
-//     const response = await fetch(Url,{
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         }
-//     });
-//     return response.json();
-// }
+async function getChannel(param=undefined){
+    Url = 'http://oreumi.appspot.com/channel/getChannelVideo?video_channel=oreumi'
+    const response = await fetch(Url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    return response.json();
+}
 
-// async function getVideo(id){
-//     url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
-//     const response = await fetch(url);
-//     return response.json();
-// }
+async function getVideo(id){
+    url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${id}`;
+    const response = await fetch(url);
+    return response.json();
+}
 
-// async function getVideoList(){
-//     url = 'http://oreumi.appspot.com/video/getVideoList';
-//     const response = await fetch(url);
-//     return response.json();
-// }
+async function getVideoList(){
+    url = 'http://oreumi.appspot.com/video/getVideoList';
+    const response = await fetch(url);
+    return response.json();
+}
 
 // function render(res){
 //     res.forEach(data){
@@ -36,8 +36,8 @@
 //         `;
 //     }
 // }
-import * as res from './requests.js';
 
-console.log(res.getVideoList());
-console.log(res.getVideo(0));
-console.log(res.getChannel());
+
+console.log(getVideoList());
+console.log(getVideo(0));
+console.log(getChannel());
