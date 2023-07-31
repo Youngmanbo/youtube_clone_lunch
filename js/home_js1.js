6968e30c57ad618406c66b37f460a3c136693ff6
kimziho <rlawlgh1027@naver.com>
2023-07-31 14:29:56 +0900

    home, channel nav display 기능 수정

--- js/home_js1.js
+++ js/home_js1.js
@@ -60,3 +60,30 @@ let searchLink = "https://www.youtube.com/results?search_query=";
 
 // 페이지 로드 시 동영상 컨테이너를 채우는 함수를 호출합니다.
 document.addEventListener("DOMContentLoaded", populateVideoContainer);
+
+
+
+
+// 메뉴 클릭시 보이고 안보이게
+imgtag = document.getElementsByTagName('img');
+menu_logo = imgtag[0];
+menu_logo.addEventListener('click' ,nav_display);
+
+function nav_display(){  
+  let nav = document.getElementsByClassName('channel-left-nav')[0];
+  let navStyle = getComputedStyle(nav).display;
+
+  let filter = document.getElementsByClassName("filter-lists")[0];
+  let bodyContainer = document.getElementsByClassName("body-container")[0];
+  if (navStyle == "none"){
+      nav.style.display = "block";
+      // 다른부분들 밀기
+      filter.style.left = "240px";
+      bodyContainer.style.marginLeft = "240px";
+    }else{
+      nav.style.display = "none";
+      // 다른부분들 당겨오기
+      filter.style.left = "0px";
+      bodyContainer.style.marginLeft = "0px";
+  }
+}
