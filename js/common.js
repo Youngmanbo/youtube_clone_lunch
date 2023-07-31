289ec62931f6142569e2e2e0b0da01d77ebfdee9
kimziho <rlawlgh1027@naver.com>
2023-07-31 11:30:40 +0900

    Add youtube logo, menu_logo click Event

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
-menu_logo = document.getElementById('home-logo').childNodes[5];
-youtube_logo = document.getElementById('home-logo').childNodes[7];
+imgtag = document.getElementsByTagName('img');
+menu_logo = imgtag[0];
+youtube_logo = imgtag[1];
 
+youtube_logo.addEventListener('click', go_home);
 menu_logo.addEventListener('click' ,nav_display);
 
-function nav_display(){
+function go_home(){
+    let curruntUrl = window.location.href;
+    let split_url = curruntUrl.split("html")[0];
+    newUrl = split_url + "html/home.html";
+    window.location.replace(newUrl);
+}
+
+// 메뉴 클릭시 보이고 안보이게
+function nav_display(){  
     let nav = document.getElementsByClassName('channel-left-nav')[0];
     if (nav.style.display == "block" || nav.style.display == ""){
         nav.style.display = "none";
