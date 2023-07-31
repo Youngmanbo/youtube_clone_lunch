efe08a59835762819d53dc9dffd8e9f8224130fd
kimziho <rlawlgh1027@naver.com>
2023-07-31 11:11:22 +0900

    Add menu_logo click eventLister

--- js/common.js
+++ js/common.js
@@ -1,2 +1,13 @@
-menu_logo = document.getElementById('home-logo').childNodes[0];
-youtube_logo = document.getElementById('home-logo').childNodes[1];
+menu_logo = document.getElementById('home-logo').childNodes[5];
+youtube_logo = document.getElementById('home-logo').childNodes[7];
+
+menu_logo.addEventListener('click' ,nav_display);
+
+function nav_display(){
+    let nav = document.getElementsByClassName('channel-left-nav')[0];
+    if (nav.style.display == "block" || nav.style.display == ""){
+        nav.style.display = "none";
+    }else{
+        nav.style.display = "block";
+    }
+}
