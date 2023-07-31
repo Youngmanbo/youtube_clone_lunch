79981302555009b43d600ab71bcbb2807c26449a
yumi choi <skgishaf@naver.com>
2023-07-31 11:50:06 +0900

    video id url불러오는 함수 생성

---
+++ js/video.js
@@ -0,0 +1,41 @@
+//vido id값
+async function getVideoList(){
+    url='http://oreumi.appspot.com/video/getVideoList';
+    const response = await fetch(url);
+    return response.json();
+}
+
+//video id값으로 url들
+async function getVideoInfoList(res){
+
+        return res.then(data =>{
+            const promises = data.map(async data=>{
+                return await getVideo(data.video_id);
+            });
+            return Promise.all(promise);
+        });
+}
+
+//메인 비디오
+async function renderChnnelVideo(res){
+    let parent = documnet.querySelector(".main-video")
+    let html = `
+        <div class='video-mainVideo'>
+            <video src=${res.video_link} poster=${res.image_link} controls></video>
+        <div>
+        <div class='video-mainInfo'>
+            <h5>${res.video_tag}</h5><br></br>
+            <h3>${res.video_title}</h3><br></br>
+            <h6>${res.views} ${res.upload_date}</h6>
+        </div>
+        `
+        parent.innerHTML=html;
+}
+
+
+
+
+
+window.onload=function(){
+    let videoInfos=getVideoInfoList();
+}
