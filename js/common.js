bb6995107af724b063090dd2762c1091cce5aaca
kimziho <rlawlgh1027@naver.com>
2023-08-04 09:25:01 +0900

    common.js 경로 수정

--- js/common.js
+++ js/common.js
@@ -6,6 +6,6 @@ youtube_logo.addEventListener('click', go_home);
 function go_home(){
     let curruntUrl = window.location.href;
     let split_url = curruntUrl.split("html")[0];
-    newUrl = split_url + "html/index.html";
+    newUrl = split_url + "index.html";
     window.location.replace(newUrl);
 }
