22b6875a23158c50c273b12397185341314e5fce
kimziho <rlawlgh1027@naver.com>
2023-08-04 09:16:26 +0900

    index.html 로 변경

--- js/common.js
+++ js/common.js
@@ -6,6 +6,6 @@ youtube_logo.addEventListener('click', go_home);
 function go_home(){
     let curruntUrl = window.location.href;
     let split_url = curruntUrl.split("html")[0];
-    newUrl = split_url + "html/home.html";
+    newUrl = split_url + "html/index.html";
     window.location.replace(newUrl);
 }
