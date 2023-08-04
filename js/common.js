dae8f22ed33cdf32f6f029ff2d952793035c682c
Youngmanbo <yo900326@gmail.com>
2023-08-04 10:00:52 +0900

    common.js 수정

--- js/common.js
+++ js/common.js
@@ -6,6 +6,7 @@ youtube_logo.addEventListener('click', go_home);
 function go_home(){
     let curruntUrl = window.location.href;
     let split_url = curruntUrl.split("youtube_clone_lunch")[0];
-    newUrl = split_url + "/index.html";
+    newUrl = split_url + "youtube_clone_lunch/index.html";
+
     window.location.replace(newUrl);
 }
