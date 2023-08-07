010f1603b018f6e4930dfdf7b74441886f4bc6bf
Youngmanbo <yo900326@gmail.com>
2023-08-07 16:41:13 +0900

    필터기능 추가 50%

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,33 @@
+let currentFilterIndex = 0;
+const filtersPerPage = 5;
 
-
-async function createFilterBtn (tag){
-
-    let parent = document.querySelector(".filter-lists");
-    let btn = document.createElement("button");
-    btn.className = 'filters';
-    btn.innerText = `${tag}`;
-
-    btn.addEventListener('click', e =>{
-        let videoList = saveDataToSessionStorage('videoList', getVideoList);
-        searchFilter(videoList, tag);
-    })
-    parent.appendChild(btn);
+function showFilters() {
+    const filterButtons = document.querySelectorAll('.filters');
+    filterButtons.forEach((button, index) => {
+        if (index >= currentFilterIndex && index < currentFilterIndex + filtersPerPage) {
+            button.classList.remove('hidden');
+            setTimeout(() => {
+                button.style.transform = 'translateX(0)';
+            }, index * 1);
+        } else {
+            button.classList.add('hidden');
+            button.style.transform = 'translateX(-30px)';
+        }
+    });
 }
+function showNextFilters() {
+    const totalFilters = document.querySelectorAll('.filters').length;
+    if (currentFilterIndex + filtersPerPage < totalFilters) {
+        currentFilterIndex += filtersPerPage;
+        showFilters();
+    }
+}
+
+function showPreviousFilters() {
+    if (currentFilterIndex - filtersPerPage >= 0) {
+        currentFilterIndex -= filtersPerPage;
+        showFilters();
+    }
+}
+
+showFilters();
